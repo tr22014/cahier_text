12714f0ba37f95e9c9f474344f1ec44990460d18
Taha Rami <126281803+tr22014@users.noreply.github.com>
2024-11-12 11:15:41 +0100

    Update database.php

--- database.php
+++ database.php
@@ -4,7 +4,7 @@ $username = "root";
 $password = "";
 $dbname = "DB";
 
-$conn = mysqli_connect($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password ,$dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
